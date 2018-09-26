b245aa889777538146371df0468a57533a4fb5e0
Ingenico ePayments <jenkins@isaac.nl>
2018-09-26 14:44:01 +0200

    Release 3.14.0.

--- IngenicoConnectSDK/ICUtil.m
+++ IngenicoConnectSDK/ICUtil.m
@@ -33,7 +33,7 @@
         NSString *deviceType = [self deviceType];
         self.metaInfo = @{
             @"platformIdentifier": platformIdentifier,
-            @"sdkIdentifier": @"iOSClientSDK/v3.13.0",
+            @"sdkIdentifier": @"iOSClientSDK/v3.14.0",
             @"sdkCreator": @"Ingenico",
             @"screenSize": screenSize,
             @"deviceBrand": @"Apple",
