df5d2559ea8e9729dce33a4617b3a5f9511f1ddd
Ingenico ePayments <github@epay.ingenico.com>
2023-05-03 14:23:06 +0200

    Release 5.18.0.

--- IngenicoConnectSDK/ICUtil.m
+++ IngenicoConnectSDK/ICUtil.m
@@ -33,7 +33,7 @@
         NSString *deviceType = [self deviceType];
         self.metaInfo = @{
             @"platformIdentifier": platformIdentifier,
-            @"sdkIdentifier": @"iOSClientSDK/v5.17.0",
+            @"sdkIdentifier": @"iOSClientSDK/v5.18.0",
             @"sdkCreator": @"Ingenico",
             @"screenSize": screenSize,
             @"deviceBrand": @"Apple",
